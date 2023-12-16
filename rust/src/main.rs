5e7cee83ec41b6948b25adf6b38575476b9cc23f
ainmx <xxainmxx@protonmail.com>
2023-12-16 05:21:39 +0100

    Feat basic rust example

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
+use sqlx::{ postgres::PgPoolOptions, mysql::MySqlPoolOptions };
 
+#[tokio::main]
+async fn main() -> Result<(), sqlx::Error> {
+    let pool = PgPoolOptions::new()
+        .connect("postgres://hsdb:hsdbpass@localhost:5432/hstestdb")
+        .await?;
+
+    let result: Vec<(String, String)> = sqlx::query_as("SELECT * FROM HTML")
+        .fetch_all(&pool)
+        .await?;
+
+    for row in result {
+        println!("{} {}", row.0, row.1);
+    }
+
+    Ok(())
 }
