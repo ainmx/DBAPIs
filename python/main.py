d7a6f6b34d6a3dab4c1a13de1024445476b306a2
CornFlaekk <roy062002@gmail.com>
2023-12-20 11:32:13 +0100

    Feat more python examples

--- python/main.py
+++ python/main.py
@@ -1,29 +1,40 @@
 import psycopg2, mysql.connector
 
-# PostgreSQL
-connection = psycopg2.connect(
-                                database = "hstestdb",
-                                host = "localhost",
-                                user = "hsdb",
-                                password = "hsdbpass",
-                                port = 5432 )
+class Html:
+    
+    def __init__(self, uuid, content) :
+        self.uuid = uuid
+        self.content = content
+        
+    def __str__(self):
+        return f"uuid: {self.uuid}\ncontent: {self.content}"
 
-cursor = connection.cursor()
 
-cursor.execute("SELECT * FROM HTML")
+def list_html(cursor):
+    cursor.execute("SELECT * FROM HTML")
+    results = cursor.fetchall()
+    
+    for result in results:
+        html = Html( result[0], result[1] );
+        print(str(html))
 
-print(cursor.fetchall())
 
-# MySQL
-connection = mysql.connector.connect(
-                                database = "hstestdb",
-                                host = "localhost",
-                                user = "hsdb",
-                                password = "hsdbpass",
-                                port = 3306 )
-
-cursor = connection.cursor()
-
-cursor.execute("SELECT * FROM HTML")
-
-print(cursor.fetchall())
+def get_page(cursor, uuid):
+    cursor.execute("SELECT * FROM HTML WHERE uuid=%s", (uuid, ))
+    result = cursor.fetchone()
+    html = Html(result[0], result[1])
+    return html
+    
+    
+if __name__ == "__main__":
+    
+    uuid = "550e8400-e29b-41d4-a716-446655440000"
+    
+    mysql_conn = mysql.connector.connect(database = "hstestdb", host = "localhost", user = "hsdb", password = "hsdbpass", port = 3306)
+    postgresql_conn = psycopg2.connect(database = "hstestdb", host = "localhost", user = "hsdb", password = "hsdbpass", port = 5432)
+    
+    connection = postgresql_conn
+    cursor = connection.cursor()
+    
+    print(get_page(cursor, uuid))
+    list_html(cursor)
